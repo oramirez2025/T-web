2aa38c965f266d4e1a6cb550e52f06dedf6a9478
Oswaldo Ramirez <oramirez2025@Oswaldos-MacBook-Air.local>
2022-06-18 07:57:40 -0500

    Progress 1

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 # import flask libraries
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 app = Flask(__name__)
-
 # flask run -> server runs at http://127.0.0.1:5000 
 @app.route("/")
-def hello():
-    return "Welcome to the home page!"
+def homepage():
+    return render_template("index.html")
+@app.route("/selection")
+def selection():
+    return render_template("selection.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
